c86df407e49d66f39592afd3c6972f851e2ade21
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-09-02 22:33:29 +0530

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Checkout from './components/Checkout/Checkout';
 
 function App() {
-  return <div className='App'>
-    <Header />
-    <Home />
-  </div>
+  return (
+    <div className="App">
+      <Router>
 
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+        
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Header.css"
+import { Link } from 'react-router-dom';
 
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
@@ -7,32 +8,39 @@ import SearchIcon from '@mui/icons-material/Search';
 
 export default function () {
   return (
-    <div className='header'> 
-        <div className="header__logo">
-      <StorefrontIcon className='header__logoImage' fontSize='large'/>
-      <h2 className="headerLogoTitle">eShop</h2>
-    </div>
+    <div className='header'>
 
-    <div className="header__search">
-      <input type="text" className='header__searchInput' />
-      <SearchIcon className='header__searchIcon' />
-    </div>
+      <Link to="/" style={{textDecoration: "none"}}>
+      <div className="header__logo">
+        <StorefrontIcon className='header__logoImage' fontSize='large' />
+        <h2 className="headerLogoTitle">eShop</h2>
+      </div>
+      </Link>
+      
+      <div className="header__search">
+        <input type="text" className='header__searchInput' />
+        <SearchIcon className='header__searchIcon' />
+      </div>
 
-    <div className="header__nav">
-      <div className="nav__item">
-        <span className="nav__itemLineOne">Hello Guest</span>
-        <span className="nav__itemLineTwo">Sign in</span>
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign in</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <div className="nav__itemBasket">
+            <ShoppingBasketIcon />
+            <span className="nav__itemLineTwo nav__basketCount">0</span>
+          </div>
+        </Link>
+        
       </div>
-      <div className="nav__item">
-        <span className="nav__itemLineOne">Your</span>
-        <span className="nav__itemLineTwo">Shop</span>
-      </div>
-      <div className="nav__itemBasket">
-        <ShoppingBasketIcon />
-        <span className="nav__itemLineTwo nav__basketCount">0</span>
-      </div>
-    </div>
     </div>
   )
-} 
- 
+}
